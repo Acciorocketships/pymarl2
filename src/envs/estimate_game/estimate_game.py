a6aa9b69843e7c4ec3304965d97d26d9d3ead013
Ryan Kortvelesy <rk627@cam.ac.uk>
2022-03-07 19:29:05 +0000

    fixed estimate

--- src/envs/estimate_game/estimate_game.py
+++ src/envs/estimate_game/estimate_game.py
@@ -7,15 +7,16 @@ class EstimateGame(MultiAgentEnv):
 		# Params
 		self.batch_mode = (batch_size != None)
 		self.n_agents = n_agents
-		self.batch_size = batch_size if not self.batch_mode else 1
+		self.batch_size = batch_size if self.batch_mode else 1
 		self.mixfunc = lambda x: np.min(x, axis=-1)
 		self.n_actions = 10
-		self.episode_length = 1
+		self.episode_limit = 1
 		self.density = 0.2
 		self.localperc = 0.3
 
 		self.state = None
 		self.adjacency = None
+		self.t = 0
 
 
 	def local_reward(self, state, action):
@@ -64,7 +65,7 @@ class EstimateGame(MultiAgentEnv):
 	def rectify_actions(self, actions):
 		# actions: n_agents x batch
 		if not self.batch_mode:
-			actions= actions[None,:]
+			actions = actions[:,None]
 		assert actions.shape[0] == self.n_agents and actions.shape[1] == self.batch_size, "incorrect actions dimensions"
 		actions = actions.transpose((1,0))[:,:,None]
 		# actions: batch x n_agents x 1
@@ -77,7 +78,7 @@ class EstimateGame(MultiAgentEnv):
 		mixed_state = self.mix_states(self.state, self.adjacency)
 		qind = self.local_reward(mixed_state, actions)[:,:,0]
 		qglobal = self.mixfunc(qind)
-		terminated = [(self.t >= self.episode_length) for _ in range(self.batch_size)]
+		terminated = [(self.t >= self.episode_limit) for _ in range(self.batch_size)]
 		if not self.batch_mode:
 			qglobal = qglobal[0].item()
 			terminated = terminated[0]
@@ -87,6 +88,8 @@ class EstimateGame(MultiAgentEnv):
 	def reset(self):
 		self.state = self.gen_state()
 		self.adjacency = self.gen_adjacency()
+		self.t = 0
+		return self.get_obs()
 
 
 	def get_obs_agent(self, agent_id, batch=0):
